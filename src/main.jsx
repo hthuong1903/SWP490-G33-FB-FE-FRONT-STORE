53a41c8d7ef0c70ad1a5aef5a5051565032e480e
hthuong1903 <thuongnth1903@gmail.com>
2022-11-06 05:58:57 +0700

    config: tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { theme } from './assets/theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    limit={3}
+                />
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 )
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    important: true,
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                ming: '#1C6E7E',
+                cultured: '#f8f8f8'
+            },
+            borderRadius: {
+                DEFAULT: '15px'
+            }
+        }
+    },
+    plugins: []
+}
